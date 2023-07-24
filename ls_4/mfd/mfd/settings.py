6a4a903cff2763d3d1a55f0be9449075cd74d931
Molodic <den041006@yandex.ru>
2023-07-24 23:11:53 +0500

    Add postgresql

--- ls_4/mfd/mfd/settings.py
+++ ls_4/mfd/mfd/settings.py
@@ -78,7 +78,15 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    },
+    'pgsql': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    },
 }
 
 
