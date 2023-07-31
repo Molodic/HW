108e494ab7d1685ca94e586c554c894434122370
Molodic <den041006@yandex.ru>
2023-08-01 00:03:49 +0500

    Add gitingore file

--- ls_4/mfd/app_lesson_4/models.py
+++ ls_4/mfd/app_lesson_4/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Advertisement(models.Model): 
+    title = models.CharField("Заголовок", max_length=128)
+    description = models.TextField("Описание")
+    price = models.DecimalField("Цена", max_digits=10, decimal_places=2)
+    tradePossibility =  models.BooleanField("Торга", help_text="Отметьте, уместен ли торг")
+    timeOfCreate = models.DateTimeField(auto_now_add = True)
+    timeOfUpdate = models.DateTimeField(auto_now = True)
