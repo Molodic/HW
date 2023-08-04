f807fafc1ae89f495af45b75393b949939319e45
Molodic <den041006@yandex.ru>
2023-08-04 14:38:30 +0500

    Task completed!

--- ls_4/mfd/app_lesson_4/models.py
+++ ls_4/mfd/app_lesson_4/models.py
@@ -2,9 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Advertisement(models.Model): 
-    title = models.CharField("Заголовок", max_length=128)
-    description = models.TextField("Описание")
-    price = models.DecimalField("Цена", max_digits=10, decimal_places=2)
-    tradePossibility =  models.BooleanField("Торга", help_text="Отметьте, уместен ли торг")
+    title: str = models.CharField("Заголовок", max_length=128)
+    description: str = models.TextField("Описание")
+    price: float = models.DecimalField("Цена", max_digits=10, decimal_places=2)
+    tradePossibility: bool =  models.BooleanField("Торга", help_text="Отметьте, уместен ли торг")
     timeOfCreate = models.DateTimeField(auto_now_add = True)
     timeOfUpdate = models.DateTimeField(auto_now = True)
+    
+    class Meta:
+        db_table = "advertisements"
+
+    def __str__(self):
+        name = str(self.__class__)
+        index = len(name) - name[::-1].find('.')
+        return f"{name[index:-2]}(id={self.id}, title={self.title}, price={self.price})"
