52eb644d4544a484944bd0fa72464a0cd56a6ce7
Molodic <den041006@yandex.ru>
2023-08-05 18:19:28 +0500

    set admin

--- ls_4/mfd/app_lesson_4/admin.py
+++ ls_4/mfd/app_lesson_4/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from .models import Advertisement
+from django.db import models #For decimal.Decimal class
 
 # Register your models here.
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'price', 'tradePossibility', 'createdDate', 'timeOfUpdate']
+    list_filter = ['tradePossibility', 'timeOfCreate']
+    list_editable = ['title', 'price', 'tradePossibility']
+    actions = ['make_trade_False', 'make_trade_True', 'sale']
+    fieldsets = (
+        ('Общие', {
+            'fields' : ('title', 'description')
+        }), 
+        ('Финансы', {
+            'fields' : ('price', 'tradePossibility'),
+            'classes' : ['collapse']
+        }), 
+        )
+    
+    @admin.action(description='Добавить возможность торга')
+    def make_trade_True(self, request, queryset):
+        queryset.update(tradePossibility = True)
+        for obj in queryset:
+            obj.save()
+
+    @admin.action(description='Убрать возможность торга')
+    def make_trade_False(self, request, queryset):
+        queryset.update(tradePossibility = False)
+        for obj in queryset:
+            obj.save()
+
+    @admin.action(description='Скидка 17 процентов')
+    def sale(self, request, queryset):
+        for obj in queryset:
+            #print(queryset[0].price-2, float(round(queryset[0].price*0.83, 2)), '---------------------------------------------------------')
+            queryset.update(price = obj.price - 2)
+            obj.save()
+
+    sale.short_description = "Распродажа!"
+
+admin.site.register(Advertisement, AdvertisementAdmin)
--- ls_4/mfd/app_lesson_4/models.py
+++ ls_4/mfd/app_lesson_4/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib import admin
+from django.utils.html import format_html
 
 # Create your models here.
 class Advertisement(models.Model): 
@@ -6,9 +8,19 @@ class Advertisement(models.Model):
     description: str = models.TextField("Описание")
     price: float = models.DecimalField("Цена", max_digits=10, decimal_places=2)
     tradePossibility: bool =  models.BooleanField("Торга", help_text="Отметьте, уместен ли торг")
-    timeOfCreate = models.DateTimeField(auto_now_add = True)
-    timeOfUpdate = models.DateTimeField(auto_now = True)
+    timeOfCreate = models.DateTimeField(verbose_name='Время создания',auto_now_add = True)
+    timeOfUpdate = models.DateTimeField(verbose_name='Время обновления', auto_now = True)
     
+    @admin.display(description="Дата создания")
+    def createdDate(self):
+        from django.utils import timezone
+        created_time = self.timeOfCreate.strftime('%H:%M:%S')
+        if self.timeOfCreate.date() == timezone.now().date():
+            return format_html(
+                "<span style='color: green; font-weight: bold;'>Сегодня в {}</span>", created_time
+            )
+        return self.timeOfCreate.strftime('%d.%m.%Y')# в %H:%M:%S')
+
     class Meta:
         db_table = "advertisements"
 
